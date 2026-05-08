dbcd4f5ffada5b25e6c70e97eb96098130447847
Evandeilson 'Lobão' Silva <esilva.code@outlook.com>
2026-05-08 18:21:27 -0300

    Refactor ZVX initialization to ensure proper module and function setup

--- shared/main.lua
+++ shared/main.lua
@@ -1,18 +1,18 @@
-ZVX = ZVX or {}
+ZVX.Logger = ZVX.Logger or {}
 
 ZVX.Name = 'Zorvax Core'
 ZVX.Author = 'Zorvax Labs'
 ZVX.Version = '0.0.1-alpha'
 ZVX.Debug = true
 
-ZVX.Modules = {}
-ZVX.Functions = {}
-ZVX.Players = {}
-ZVX.Callbacks = {}
-ZVX.Events = {}
-ZVX.Database = {}
-ZVX.Logger = {}
-ZVX.Cache = {}
+ZVX.Modules = ZVX.Modules or {}
+ZVX.Functions = ZVX.Functions or {}
+ZVX.Players = ZVX.Players or {}
+ZVX.Callbacks = ZVX.Callbacks or {}
+ZVX.Events = ZVX.Events or {}
+ZVX.Database = ZVX.Database or {}
+ZVX.Logger = ZVX.Logger or {}
+ZVX.Cache = ZVX.Cache or {}
 
 ZVX.Environment = {
     Resource = GetCurrentResourceName(),
