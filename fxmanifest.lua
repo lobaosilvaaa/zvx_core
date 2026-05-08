0aa53869e40db9f6db2c48cd9c7957727a063ef2
Evandeilson 'Lobão' Silva <esilva.code@outlook.com>
2026-05-07 21:14:47 -0300

    Add initial project files including CODE_OF_CONDUCT, CONTRIBUTING, ROADMAP, and core Lua scripts

---
+++ fxmanifest.lua
@@ -0,0 +1,30 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+name 'zvx_core'
+author 'Zorvax Labs'
+description 'Modern modular framework for FiveM.'
+version '0.0.1-alpha'
+
+shared_scripts {
+    'shared/version.lua',
+    'shared/constants.lua',
+    'shared/functions.lua',
+    'shared/main.lua',
+    
+    'config/config.lua'
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    'server/main.lua'
+}
+
+dependencies {
+    '/server:7290'
+}
