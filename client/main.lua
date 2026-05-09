6e2936bfd568e20db064268a8c5dff17fab52068
Evandeilson 'Lobão' Silva <esilva.code@outlook.com>
2026-05-09 15:52:59 -0300

    Refactor callback logging for improved clarity and consistency

--- client/main.lua
+++ client/main.lua
@@ -2,6 +2,8 @@ CreateThread(function()
     Wait(3000)
 
     ZVX.Callbacks.Trigger('zvx:test:ping', function(response)
-        print(('[CLIENT] Callback response: %s'):format(response))
+        print(('[CLIENT] Callback response: %s'):format(
+            response
+        ))
     end)
 end)
--- modules/callbacks/client.lua
+++ modules/callbacks/client.lua
@@ -15,16 +15,28 @@ function ZVX.Callbacks.Trigger(name, callback, ...)
         current_request_id,
         ...
     )
+
+    ZVX.Logger.Debug((
+        'Triggered callback request: %s'
+    ):format(name))
 end
 
 RegisterNetEvent('zvx:callbacks:response', function(request_id, ...)
     local request_callback = pending_requests[request_id]
 
     if not request_callback then
+        ZVX.Logger.Warn((
+            'Received invalid callback response: %s'
+        ):format(request_id))
+
         return
     end
 
     request_callback(...)
 
     pending_requests[request_id] = nil
+
+    ZVX.Logger.Debug((
+        'Completed callback request: %s'
+    ):format(request_id))
 end)
